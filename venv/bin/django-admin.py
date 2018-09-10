043865bb7c5233f919017d2384ff79b671d83a27
v13ar14ls <v13aer14ls@gmail.com>
2018-09-10 17:21:06 -0300

    coisando o django

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,7 @@
+#!/home/peter/moon/estudos/Django_Basico/venv/bin/python3
+# EASY-INSTALL-DEV-SCRIPT: 'Django==2.2.dev20180910200821','django-admin.py'
+__requires__ = 'Django==2.2.dev20180910200821'
+__import__('pkg_resources').require('Django==2.2.dev20180910200821')
+__file__ = '/home/peter/moon/estudos/Django_Basico/django/django/bin/django-admin.py'
+with open(__file__) as f:
+    exec(compile(f.read(), __file__, 'exec'))
